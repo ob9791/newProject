883b993f9f4afc1a271020316f24dd7b22c8c85f
Ob <oblidi17@gmail.com>
2022-06-06 17:03:23 +0100

    added sonarcloud

--- src/test/java/com/tsi/o/blidi/program/MyFirstMicroServiceApplicationTests.java
+++ src/test/java/com/tsi/o/blidi/program/MyFirstMicroServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.tsi.o.blidi.program;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MyFirstMicroServiceApplicationTests {
 
 	@Test
